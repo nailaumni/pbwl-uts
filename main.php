7645c6424d2a58dd83d2b5e52c8d743e83800a22
nailaumni <118437339+nailaumni@users.noreply.github.com>
2023-05-30 00:25:05 +0700

    first

---
+++ main.php
@@ -0,0 +1,4 @@
+<h2>Home</h2>
+
+<div class="info">Selamat datang, <strong>Borcelle</strong></div>
+<div>Kue memiliki arti makanan yg dibuat dari bahan yg bermacam-macam, dapat dibuat dari berbagai bentuk. Pada toko Borcelle kami menjual berbagai macam aneka kue kering. Kue kering adalah jenis kue di Indonesia yang tidak mengandung air. Bahan untuk adonan kue kering terbagi menjadi bahan utama dan bahan tambahan. Bahan utamanya adalah tepung terigu, gula, telur dan bahan pengembang. Sedangkan bahan tambahannya antara lain buah-buahan dan kacang-kacangan. Kue kering adalah salah satu makanan yang biasanya selalu ada saat suasana lebaran. </div>
